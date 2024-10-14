97bcc1104347cd9e6b637dd3c734bdd033d22c1e
Juan Pablo Leon Montenegro <juanpablo.leonm15@gmail.com>
2024-10-14 12:49:17 -0500

    Finalizado

--- src/App.java
+++ src/App.java
@@ -6,18 +6,12 @@ public class App {
 
         MetodosBusqueda metodosBusqueda = new MetodosBusqueda();
 
+        // Llenar el arreglo con valores del 1 al 100000
         int[] arreglo = new int[100000]; 
-        for(int i = 0; i < 100000; i++){
+        for (int i = 0; i < 100000; i++) {
             arreglo[i] = i + 1;
         }
-
-        int[] arr = new int[100000]; 
-        for(int i = 0; i < 100000; i++){
-            arreglo[i] = i + 1;
-        }
-        metodosBusqueda.printArreglo(arreglo);
-
-        int posicion = metodosBusqueda.busquedaBinaria(arr, 50418);
+        int posicion = metodosBusqueda.busquedaBinaria(arreglo, 50418);
 
         if (posicion != -1) {
             System.out.println("Con el metodo Binario El valor se encuentra en la posicion " + posicion);
@@ -25,15 +19,14 @@ public class App {
             System.out.println("Valor no encontrado 404");
         }
 
-
+        // Búsqueda Secuencial
         int posicion1 = metodosBusqueda.busquedaSecuencial(arreglo, 50418);
 
         if (posicion1 != -1) {
             System.out.println("Con el metodo Secuencial El valor se encuentra en la posicion " + posicion1);
-            System.out.println("Se realizaron " + (posicion1+1) + " iteraciones.");
+            System.out.println("Se realizaron " + (posicion1 + 1) + " iteraciones.");
         } else {
             System.out.println("Valor no encontrado 404");
         }
     }
 }
-
